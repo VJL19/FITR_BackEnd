118e496571d7c32b21affcde97c17edaafeae3ae
IamNEO <Llauderes@c1@gmail.com>
2024-11-13 22:46:52 +0800

    configure mysql file

--- config/mysql.ts
+++ config/mysql.ts
@@ -6,6 +6,7 @@ const config = loadConfig();
 const connection = mysql.createConnection({
   host: config.host,
   user: config.user,
+  port: config.port,
   password: config.password,
   database: config.database,
   multipleStatements: true,
